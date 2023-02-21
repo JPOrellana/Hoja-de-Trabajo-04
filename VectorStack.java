378134a8b8c458aeaf2a3274f1809a8145d5604b
JPOrellana <115191781+JPOrellana@users.noreply.github.com>
2023-02-20 21:45:03 -0600

    Creación del VectorStack

---
+++ VectorStack.java
@@ -0,0 +1,67 @@
+import java.util.Vector;
+
+public class VectorStack<T> extends Stack<T>{
+    private Vector<T> core;
+        
+        /***
+         * Metodo constructor de Stack
+         */
+        public VectorStack() {
+            core = new Vector<T>();
+        }
+        
+        @Override
+        /**
+         * Ingresa un valor a la pila
+         * @param value valor a ingresar
+         */
+        public void push(T value) {
+            core.add(0, value);
+            
+        }
+    
+        @Override
+        /**
+         * Saca un valor a la pila 
+         * @return Devuelve el ultimo valor de la pila
+         */
+        public T pull() {
+            if(!isEmpty()) {
+                return core.remove(0);
+            }else {
+                return null;
+            }
+            
+        }
+    
+        @Override
+        /**
+         * Muestra un valor a la pila 
+         * @return Muestra el ultimo valor de la pila
+         */
+        public T peek() {
+            if(!isEmpty()) {
+                return core.get(0);
+            }else{
+                return null;
+            }
+        }
+    
+        @Override
+        /**
+         * Cuenta los elementos de la pila
+         * @return Devuelve el tama�o de la pila
+         */
+        public int count() {
+            return core.size();
+        }
+    
+        @Override
+        /**
+         * Verifica si la pila tiene elementos
+         * @return Devuelve True si la pila esta vacia y False si no lo esta 
+         */
+        public boolean isEmpty() {
+            return core.isEmpty();
+        }
+    }
